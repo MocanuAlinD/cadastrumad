14988ba77bdc27b414c96d0fad4a7622d5f9bac8
MocanuAlinD <alin_ngt@yahoo.com>
2025-02-03 15:54:02 +0200

    Pagina tarife 02

--- src/components/Preturi/index.tsx
+++ src/components/Preturi/index.tsx
@@ -3,9 +3,39 @@ import styles from "./preturi.module.css";
 
 const Preturi = () => {
   return (
-    <div className={styles.container + " mx-auto"}>
+    <div className={styles.topContainer + " mx-auto"}>
       <div className={styles.tableContainer}>
-        {/* <h3 className="text-center">Apartamente</h3> */}
+        <table className={styles.table + " table-auto my-8 mb-16"}>
+          <tbody>
+            <tr>
+              <td className={ styles.important + " text-center font-bold text-lg sm:text-2xl"}>IMPORTANT</td>
+            </tr>
+            <tr>
+              <td
+                colSpan={3}
+                className={
+                  styles.notaSubsol +
+                  " px-2 leading-6 sm:leading-8"
+                }
+              >
+                - Beneficiați de consultanță gratuită pentru toate tipurile de
+                lucrări.
+                <br />
+                - Tarifele NU INCLUD taxele OCPI (unde se aplică taxe).
+                <br />
+                - Tarifele sunt informative și diferă în funcție de acte și
+                complexitate.
+                <br />- Tarifele afișate reprezintă valorile minimale și pot
+                varia în funcție de specificul și complexitatea fiecărei
+                lucrări. O estimare corectă va fi calculată după analiza tuturor
+                detaliilor necesare pentru realizarea proiectului.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className={styles.tableContainer}>
         <table className={styles.table + " table-auto"}>
           <thead>
             <tr>
@@ -30,24 +60,11 @@ const Preturi = () => {
               <td>300 lei/UI</td>
               <td>400 lei/UI</td>
             </tr>
-            <tr>
-              <td colSpan={3} className={styles.notaSubsol}>
-                - Tarifele NU INCLUD taxele OCPI.
-                <br />
-                - Tarifele sunt informative și diferă în funcție de acte și
-                complexitate.
-                <br />- Tarifele afișate reprezintă valorile minimale și pot
-                varia în funcție de specificul și complexitatea fiecărei
-                lucrări. O estimare corectă va fi calculată după analiza tuturor
-                detaliilor necesare pentru realizarea proiectului.
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
 
       <div className={styles.tableContainer}>
-        {/* <h3 className="text-center">Terenuri (cu/fără construcții)</h3> */}
         <table className={styles.table + " table-auto"}>
           <thead>
             <tr>
@@ -72,24 +89,11 @@ const Preturi = () => {
               <td>1000 lei + 3 lei/mp(optional)</td>
               <td>1200 lei + 3 lei/mp(optional)</td>
             </tr>
-            <tr>
-              <td colSpan={3} className={styles.notaSubsol}>
-                - Tarifele NU INCLUD taxele OCPI.
-                <br />
-                - Tarifele sunt informative și diferă în funcție de acte și
-                complexitate.
-                <br />- Tarifele afișate reprezintă valorile minimale și pot
-                varia în funcție de specificul și complexitatea fiecărei
-                lucrări. O estimare corectă va fi calculată după analiza tuturor
-                detaliilor necesare pentru realizarea proiectului.
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
 
       <div className={styles.tableContainer}>
-        {/* <h3 className="text-center">Dezmembrare/alipire</h3> */}
         <table className={styles.table + " table-auto"}>
           <thead>
             <tr>
@@ -114,24 +118,11 @@ const Preturi = () => {
               <td>300 lei/lot</td>
               <td>400 lei/lot</td>
             </tr>
-            <tr>
-              <td colSpan={3} className={styles.notaSubsol}>
-                - Tarifele NU INCLUD taxele OCPI.
-                <br />
-                - Tarifele sunt informative și diferă în funcție de acte și
-                complexitate.
-                <br />- Tarifele afișate reprezintă valorile minimale și pot
-                varia în funcție de specificul și complexitatea fiecărei
-                lucrări. O estimare corectă va fi calculată după analiza tuturor
-                detaliilor necesare pentru realizarea proiectului.
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
 
       <div className={styles.tableContainer}>
-        {/* <h3 className="text-center">Rectificare/actualizare</h3> */}
         <table className={styles.table + " table-auto"}>
           <thead>
             <tr>
@@ -154,26 +145,11 @@ const Preturi = () => {
               <td>1000 lei</td>
               <td>1200 lei</td>
             </tr>
-
-            <tr>
-              <td colSpan={3} className={styles.notaSubsol}>
-                - Tarifele NU INCLUD taxele OCPI.
-                <br />
-                - Tarifele sunt informative și diferă în funcție de acte și
-                complexitate.
-                <br />- Tarifele afișate reprezintă valorile minimale și pot
-                varia în funcție de specificul și complexitatea fiecărei
-                lucrări. O estimare corectă va fi calculată după analiza tuturor
-                detaliilor necesare pentru realizarea proiectului.
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
 
-
       <div className={styles.tableContainer}>
-        {/* <h3 className="text-center">Consultare/informare</h3> */}
         <table className={styles.table + " table-auto"}>
           <thead>
             <tr>
@@ -189,28 +165,18 @@ const Preturi = () => {
               <td>-</td>
             </tr>
             <tr>
-              <td>Obținere copii documente pierdute (cadastru, încheiere de intabulare)</td>
+              <td>
+                Obținere copii documente pierdute (cadastru, încheiere de
+                intabulare)
+              </td>
               <td>150 lei</td>
               <td>-</td>
-            </tr>
-            <tr>
-              <td colSpan={3} className={styles.notaSubsol}>
-                - Tarifele NU INCLUD taxele OCPI.
-                <br />
-                - Tarifele sunt informative și diferă în funcție de acte și
-                complexitate.
-                <br />- Tarifele afișate reprezintă valorile minimale și pot
-                varia în funcție de specificul și complexitatea fiecărei
-                lucrări. O estimare corectă va fi calculată după analiza tuturor
-                detaliilor necesare pentru realizarea proiectului.
-              </td>
             </tr>
           </tbody>
         </table>
       </div>
 
       <div className={styles.tableContainer}>
-        {/* <h3 className="text-center">Topografie</h3> */}
         <table className={styles.table + " table-auto"}>
           <thead>
             <tr>
@@ -244,118 +210,6 @@ const Preturi = () => {
               <td>Trasări axe construcții</td>
               <td>450 lei</td>
               <td>600 lei</td>
-            </tr>
-            <tr>
-              <td colSpan={3} className={styles.notaSubsol}>
-                - Tarifele NU INCLUD taxele OCPI (unde se aplică taxe).
-                <br />
-                - Tarifele sunt informative și diferă în funcție de acte și
-                complexitate.
-                <br />- Tarifele afișate reprezintă valorile minimale și pot
-                varia în funcție de specificul și complexitatea fiecărei
-                lucrări. O estimare corectă va fi calculată după analiza tuturor
-                detaliilor necesare pentru realizarea proiectului.
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <div className={styles.tableContainer}>
-        <h3 className="text-center">Apartamente</h3>
-        <table className={styles.table + " table-auto"}>
-          <thead>
-            <tr>
-              <th>Serviciu</th>
-              <th>Tarif regim normal</th>
-              <th>Tarif regim urgență</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Consultanță</td>
-              <td>GRATUIT</td>
-              <td>GRATUIT</td>
-            </tr>
-            <tr>
-              <td>Cadastru apartament 1-2 camere</td>
-              <td>650 lei</td>
-              <td>750 lei</td>
-            </tr>
-            <tr>
-              <td>Cadastru apartament 3-4 camere</td>
-              <td>800 lei</td>
-              <td>950 lei</td>
-            </tr>
-            <tr>
-              <td>Cadastru teren</td>
-              <td>1500 lei</td>
-              <td>1700 lei</td>
-            </tr>
-            <tr>
-              <td>Cadastru teren + casă</td>
-              <td>1600 lei+3 lei/mp releveu</td>
-              <td>1800 lei+3 lei/mp releveu</td>
-            </tr>
-            <tr>
-              <td>Actualizare informații tehnice</td>
-              <td>1000 lei</td>
-              <td>1200 lei</td>
-            </tr>
-            <tr>
-              <td>
-                Rectificare (modificarea limitei de proprietate sau a suprafeței
-                terenului)
-              </td>
-              <td>1000 lei</td>
-              <td>1200 lei</td>
-            </tr>
-            <tr>
-              <td>Înscriere construcție nouă</td>
-              <td>1000 lei + 3 lei/mp(optional)</td>
-              <td>1200 lei + 3 lei/mp(optional)</td>
-            </tr>
-            <tr>
-              <td>Dezmembrare</td>
-              <td>300 lei/lot</td>
-              <td>400 lei/lot</td>
-            </tr>
-            <tr>
-              <td>Apartamentare</td>
-              <td>300 lei/UI</td>
-              <td>400 lei/UI</td>
-            </tr>
-            <tr>
-              <td>Alipire(comasare)</td>
-              <td>300 lei/lot</td>
-              <td>400 lei/lot</td>
-            </tr>
-            <tr>
-              <td>Plan situație</td>
-              <td>600 lei</td>
-              <td>750 lei</td>
-            </tr>
-            <tr>
-              <td>Înțărușare teren intravilan</td>
-              <td>400 lei</td>
-              <td>600 lei</td>
-            </tr>
-            <tr>
-              <td>Înțărușare teren extravilan</td>
-              <td>450 lei</td>
-              <td>650 lei</td>
-            </tr>
-            <tr>
-              <td colSpan={3} className={styles.notaSubsol}>
-                - Tarifele NU INCLUD taxele OCPI.
-                <br />
-                - Tarifele sunt informative și diferă în funcție de acte și
-                complexitate.
-                <br />- Tarifele afișate reprezintă valorile minimale și pot
-                varia în funcție de specificul și complexitatea fiecărei
-                lucrări. O estimare corectă va fi calculată după analiza tuturor
-                detaliilor necesare pentru realizarea proiectului.
-              </td>
             </tr>
           </tbody>
         </table>
