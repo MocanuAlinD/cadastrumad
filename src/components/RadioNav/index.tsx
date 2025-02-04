67136c7100374f264496ab3c306d5af31f6537a0
MocanuAlinD <alin_ngt@yahoo.com>
2025-02-04 11:56:39 +0200

    Clip path to meniu button

--- src/components/RadioNav/index.tsx
+++ src/components/RadioNav/index.tsx
@@ -18,7 +18,13 @@ const Navigation = () => {
     <div className={styles.topContainer + " px-2 md:px-16 flex items-center justify-around h-full w-full"}>
       <h1 className={cx(styles.titleLogo,"cursor-pointer")} onClick={() => router.push("/")}>Cadastru</h1>
       <button className='text-black lg:hidden flex underline underline-offset-4' onClick={() => setOpen(!open)}>Meniu</button>
-      <div className={cx(styles.navContainer, open ? "flex lg:flex" : "hidden lg:flex")}>
+      <div className={styles.navContainer} 
+      style={{
+        "--clip": open
+          ? "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+          : "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)"
+      } as React.CSSProperties}
+      >
         <label className={styles.label} htmlFor="op1">
           Acasă
         </label>
@@ -44,7 +50,7 @@ const Navigation = () => {
         />
 
         <label className={styles.label} htmlFor="op3">
-          Tarife
+          Tarife & acte
         </label>
         <input
           id="op3"
@@ -55,8 +61,8 @@ const Navigation = () => {
           onChange={() => (router.push("/tarife"), setOpen(false))}
         />
 
-        <label className={styles.label} htmlFor="op4">Acte</label>
-        <input id="op4" type="radio" name="options" value="option4" checked={path === "/acte"} onChange={() => (router.push("/acte"), setOpen(false))} />
+        {/* <label className={styles.label} htmlFor="op4">Acte</label> */}
+        {/* <input id="op4" type="radio" name="options" value="option4" checked={path === "/acte"} onChange={() => (router.push("/acte"), setOpen(false))} /> */}
 
         <label className={styles.label} htmlFor="op5">Informații</label>
         <input id="op5" type="radio" name="options" value="option5" checked={path === "/info"} onChange={() => (router.push("/info"), setOpen(false))} />
