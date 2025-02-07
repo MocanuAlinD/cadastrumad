6b932588a89ad97cbfb21424c465bc8998b1e7e0
MocanuAlinD <alin_ngt@yahoo.com>
2025-02-07 14:01:27 +0200

    Galerie DONE

--- src/app/(meniu)/galerie/page.tsx
+++ src/app/(meniu)/galerie/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import ImageGalerie from "@/components/ImageGalerie";
 // import styles from "./galerie.module.css"
+import { pozeLucrari } from "@/lib/poze";
 
 const Galerie = () => {
   return (
-    <div>Galerie</div>
-  )
-}
+    <div className="w-full flex flex-wrap justify-center">
+      {pozeLucrari.map((i,index) => <ImageGalerie key={index} src={i.src} text={i.text} />)}
+    </div>
+  );
+};
 
-export default Galerie
+export default Galerie;
---
+++ src/components/ImageGalerie/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./galerie.module.css";
+
+const ImageGalerie = ({ text,src }: {src?:string, text?: string }) => {
+  return (
+    <div className={styles.container}>
+      <h4 className="w-full text-center text-xs md:text-sm font-light md:font-normal py-1">
+        &emsp;{text ? text : "Text goes here"}
+      </h4>
+      <div className={styles.imageContainer + " h-[12rem] md:h-[30rem]"}>
+        <Image
+          src={src ? src : "/globe.svg"}
+          sizes="(max-width: 768px) 50vw"
+          alt="Image"
+          layout="fill"
+          className={styles.image}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImageGalerie;
