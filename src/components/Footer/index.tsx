a237b5f757c4fcf0d3a1ca9a1cabd3d6082a2ff5
MocanuAlinD <alin_ngt@yahoo.com>
2025-02-14 13:00:21 +0200

    Footer added anpc

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   const footerColumn = `${styles.footerColumn} + " w-[100%] h-fit md:max-w-[30rem] my-[2rem] py-8 flex flex-col items-center lg:items-start`;
 
   return (
     <div className="flex flex-col items-center justify-around border-t border-[var(--background)]">
-      <h2 className={styles.title + " my-12"}>CADASTRU & INTABULARE< br/>CONSTANȚA</h2>
+      <h2 className={styles.title + " my-12"}>
+        CADASTRU & INTABULARE
+        <br />
+        CONSTANȚA
+      </h2>
       <div className="w-full flex flex-col items-center lg:items-start lg:flex-row justify-around">
         <div className={footerColumn}>
           <h3>Contact</h3>
@@ -15,12 +21,18 @@ const Footer = () => {
           </div>
           <div>
             <span>WhatsApp: </span>&emsp;
-            <a aria-label="Chat on WhatsApp" href="https://wa.me/+40754765388?text=Bună%20ziua!%20Sunt%20interesat%20de%20" >
+            <a
+              aria-label="Chat on WhatsApp"
+              href="https://wa.me/+40754765388?text=Bună%20ziua!%20Sunt%20interesat%20de%20"
+            >
               0754.765.388
             </a>
           </div>
           <div>
-            <span>Email: </span>&emsp;<a href="mailto:cadastru_mocanu@yahoo.com">cadastru_mocanu@yahoo.com</a>
+            <span>Email: </span>&emsp;
+            <a href="mailto:cadastru_mocanu@yahoo.com">
+              cadastru_mocanu@yahoo.com
+            </a>
           </div>
           <div>
             <span>Program: </span>&emsp; Luni - Vineri 10 - 18
@@ -35,9 +47,33 @@ const Footer = () => {
             <span>PFA: </span>&emsp; Mocanu Alin-Daniel
           </div>
         </div>
-        <div className={footerColumn}>col 2</div>
-        <div className={footerColumn}>TO-ADD: anpc-sal</div>
+        <div className={footerColumn}>
+          <p>Politica de confidentialitate</p>
+          <p>Cookies</p>
+          <p>Social media, instagram, facebook</p>
+        </div>
+        <div className={footerColumn}>
+          <div className="flex flex-col gap-4">
+            <Link href="https://anpc.ro/ce-este-sal">
+              <Image
+                src="/anpc-sal_01.jpg"
+                width="200"
+                height="0"
+                alt="anpc-sal"
+              />
+            </Link>
+            <Link href="https://ec.europa.eu/consumers/odr">
+            <Image
+              src="/anpc-sal_02.jpg"
+              width="200"
+              height="0"
+              alt="anpc-sal"
+              />
+              </Link>
+          </div>
+        </div>
       </div>
+      <p className='py-8 mb-4'>© Mocanu Alin-Daniel | 2025</p>
     </div>
   );
 };
