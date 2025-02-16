1075227e3422f72ff04db95898309ee17b3285dd
MocanuAlinD <alin_ngt@yahoo.com>
2025-02-16 14:51:45 +0200

    Footer in progress

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { CiFacebook } from "react-icons/ci";
 import styles from "./footer.module.css";
 
 const Footer = () => {
@@ -19,15 +19,16 @@ const Footer = () => {
         CONSTANȚA
       </h2>
       <div className="w-full flex flex-col items-center lg:items-start lg:flex-row justify-around">
-
-
         <div className={footerColumn}>
           <h3>Contact</h3>
           <div>
-            <span>Telefon:</span>&emsp;<a href="tel:0754765388">0754.765.388</a>
+            <span>Telefon</span>
+            &emsp;
+            <a href="tel:0754765388">0754.765.388</a>
           </div>
           <div>
-            <span>WhatsApp: </span>&emsp;
+            <span>WhatsApp </span>
+            &emsp;
             <a
               aria-label="Chat on WhatsApp"
               href="https://wa.me/+40754765388?text=Bună%20ziua!%20Sunt%20interesat%20de%20"
@@ -36,13 +37,24 @@ const Footer = () => {
             </a>
           </div>
           <div>
-            <span>Email: </span>&emsp;
+            <span>Email</span>
+            &emsp;
             <a href="mailto:cadastru_mocanu@yahoo.com">
               cadastru_mocanu@yahoo.com
             </a>
           </div>
           <div>
-            <span>Program: </span>&emsp; Luni - Vineri 10 - 18
+            <span>Facebook</span>
+            <span className="flex">
+              <CiFacebook className="text-2xl text-[var(--blueLight)]" />
+              <Link href="https://www.facebook.com/MocanuCadastruConstanta">
+                /MocanuCadastruConstanta
+              </Link>
+            </span>
+          </div>
+          <div>
+            <span>Program</span>
+            <span> &emsp;Luni - Vineri 10 - 18</span>
           </div>
           <div>
             <span>NRC: </span>&emsp; F40/5030/2024
@@ -56,20 +68,15 @@ const Footer = () => {
         </div>
 
         <div className={footerColumn}>
-          <Link href="/politica">
-            <p className="text-[var(--blueLight)] hover:text-blue-700">
-              Politica de confidențialitate
-            </p>
+          <h3>Linkuri utile</h3>
+          <Link href="/politica" className="text-[var(--blueLight)] hover:text-blue-700" >
+            Politica de confidențialitate
           </Link>
-          <div className="border border-red-500">
-            <p>Social media, instagram, facebook</p>
-          </div>
-          <div className="border border-red-500">
-            <p>Credits</p>
+          <div>
+            <p className='mx-auto'>Credits</p>
           </div>
         </div>
 
-        
         <div className={footerColumn}>
           <div className="flex flex-col gap-4">
             <Link href="https://anpc.ro/ce-este-sal">
